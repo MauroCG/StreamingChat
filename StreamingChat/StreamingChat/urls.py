30bf562bce206ab26da4b40d87f4cf44da996f32
MauroCG <mauro57.mauricio@gmail.com>
2019-06-10 08:37:21 -0500

    App for the login created. This is not functional yet. The next step is to add the python social auth

--- StreamingChat/StreamingChat/urls.py
+++ StreamingChat/StreamingChat/urls.py
@@ -1,21 +1,11 @@
-"""StreamingChat URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.conf.urls import include, url
 from django.urls import path
+from apps.login.views import login
+from django_private_chat import urls as django_private_chat_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include(django_private_chat_urls)),
+    path('', login, name='login'),
 ]

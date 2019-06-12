03885117f8accac5eb83da22a6a16b5c4fa4b3d6
MauroCG <mauro57.mauricio@gmail.com>
2019-06-12 18:34:56 -0500

    A new api for facebook was created and to the login views was agregated the user list view (this is not used still).

--- StreamingChat/apps/login/views.py
+++ StreamingChat/apps/login/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout
+from braces.views import LoginRequiredMixin
+from django.views import generic
+from django.contrib.auth import get_user_model
+
+
+class UserListView(LoginRequiredMixin, generic.ListView):
+    model = get_user_model()
+    # These next two lines tell the view to index lookups by username
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    template_name = 'custom_app/users.html'
+	login_url = ''
 
 def login(request):
     return render(request, 'login/login.html')
